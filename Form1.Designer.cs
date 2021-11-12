a6e224eac5fa3b77ddbf3c6281dd7dae0970ea7c
huyen27 <80885008+huyen27@users.noreply.github.com>
2021-11-12 16:10:21 +0700

    Them su kien button Cong

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -94,6 +94,7 @@ namespace Calculator
             this.btCong.TabIndex = 2;
             this.btCong.Text = "Cộng";
             this.btCong.UseVisualStyleBackColor = true;
+            this.btCong.Click += new System.EventHandler(this.btCong_Click);
             // 
             // Form1
             // 
